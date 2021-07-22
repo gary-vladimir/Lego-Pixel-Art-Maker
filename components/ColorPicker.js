aaf88d2e1bf4eefb88116190afe6a7aa037813ed
gary-vladimir <gary@garybricks.com>
2021-07-22 13:51:37 -0500

    added 12 official lego colors

--- components/ColorPicker.js
+++ components/ColorPicker.js
@@ -1,6 +1,21 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
+const legoColors = [
+    '05131D',
+    'F2CD37',
+    '237841',
+    '008F9B',
+    '0055BF',
+    'E4ADC8',
+    'ADC3C0',
+    'D09168',
+    '4B9F4A',
+    '81007B',
+    'BBE90B',
+    'FFFFFF',
+];
+
 export function ColorPicker() {
     return (
         <View style={styles.container}>
